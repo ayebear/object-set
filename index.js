c8f775e0b8445723e5418f5ca3c5026ccbf58887
Eric Hebert <ayebear@gmail.com>
2021-12-29 21:13:52 -0500

    Implement initial methods Test for add()

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ export default class ObjectSet {
   // key: function or string
   // data: optional iterable array-like
   constructor(key, data) {
-    this.key = key
-    this.items = new Map()
+    this._items = new Map()
     if (typeof key !== 'function' && typeof key !== 'string') {
       throw new Error('Must pass function or string to ObjectSet constructor')
+    }
+    if (typeof key === 'string') {
+      this._getKey = item => item[key]
+    } else {
+      this._getKey = key
     }
     if (data) {
       for (const item of data) {
@@ -15,11 +19,34 @@ export default class ObjectSet {
   }
 
   get size() {
-    return this.items.size
+    return this._items.size
   }
 
-  add() {}
-  delete() {}
-  has() {}
-  values() {}
+  add(item) {
+    this._items.set(this._getKey(item), item)
+  }
+
+  clear() {
+    this._items.clear()
+  }
+
+  delete(item) {
+    this._items.delete(this._getKey(item))
+  }
+
+  entries() {
+    return this._items.entries()
+  }
+
+  forEach(...args) {
+    return this._items.forEach(...args)
+  }
+
+  has(item) {
+    this._items.has(this._getKey(item))
+  }
+
+  values() {
+    return this._items.values()
+  }
 }
--- index.test.js
+++ index.test.js
@@ -3,12 +3,21 @@ import ObjectSet from './index.js'
 
 test('create ObjectSet', () => {
   const s1 = new ObjectSet('key')
-  const s2 = new ObjectSet(elem => {
-    elem.key
-  })
+  const s2 = new ObjectSet(elem => elem.key)
   expect(s1.size).toBe(0)
   expect(s2.size).toBe(0)
   expect(() => {
     new ObjectSet()
   }).toThrow()
+})
+
+test('add', () => {
+  const s1 = new ObjectSet('key')
+  s1.add({ key: 'foo' })
+  expect(s1.size).toBe(1)
+  const s2 = new ObjectSet(elem => `${elem.x},${elem.y}`)
+  s2.add({ x: 1, y: 2 })
+  s2.add({ x: 1, y: 2 })
+  s2.add({ x: 2, y: 2 })
+  expect(s2.size).toBe(2)
 })
